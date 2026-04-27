545ac16a8de8e020cb2a6f0991371d623c89cd61
mustaphaabbass352 <79026489+mustaphaabbass352@users.noreply.github.com>
2026-04-27 21:46:07 +0000

    new groq key

--- api/generate.js
+++ api/generate.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   const { prompt } = req.body;
-  const GROQ_KEY = "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";
+  const GROQ_KEY = "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";
 if (!GROQ_KEY) return res.status(500).json({ error: 'No GROQ key found' });
 
   try {
